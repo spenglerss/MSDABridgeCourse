5c32b436fba1e7d727df7ca7d0c5a98b49786772
spenglerss <sandeshsadalge@gmail.com>
2014-07-08 16:14:10 -0400

    Week 6 Python Assignment

--- Week 6/Week_6_Python_Assignment.py
+++ Week 6/Week_6_Python_Assignment.py
@@ -9,14 +9,7 @@ PLEASE NOTE: I put the 4 lines in a txt file called Week_6_Python_data.txt to te
 import re
 
 
-fhand = open('https://github.com/spenglerss/MSDABridgeCourse/tree/master/Week%206/Week_6_Python_data.txt')#open('C:\Users\ssadalge\Documents\GitHub\MSDABridgeCourse\Week 6\Week_6_Python_data.txt')
-print fhand
-
-print type(fhand)
-
-for line in fhand:
-    print type(line)
-
+fhand = open('C:\Users\ssadalge\Documents\GitHub\MSDABridgeCourse\Week 6\Week_6_Python_data.txt')
 
 '''  The various strings in the file:
 1 Germany  | W 3 | T 4 | W 2
@@ -25,16 +18,17 @@ for line in fhand:
 4 Ghana | L 2 | T 1 | L 3
 '''
 
-theString = '1 Germany  | W 3 | T 4 | W 2'
+for line in fhand:
+    #theString = '1 Germany  | W 3 | T 4 | W 2'
 
-match = re.search('(^[0-9]+) +([a-zA-Z ]+)\| ([WLT]+ [0-9 ]+)\| ([WLT]+ [0-9 ]+)\| ([WLT]+ [0-9 ]+)',theString)
+    match = re.search('(^[0-9]+) +([a-zA-Z ]+)\| ([WLT]+ [0-9 ]+)\| ([WLT]+ [0-9 ]+)\| ([WLT]+ [0-9 ]+)',line)
 
-if match:
-    print "Rank: ",match.group(1)
-    print "Country: ",match.group(2)
-    print "Results:"
-    print match.group(3)
-    print match.group(4)
-    print match.group(5)
-else:
-    print "No Match"
+    if match:
+        print "Rank: ",match.group(1)
+        print "Country: ",match.group(2)
+        print "Results:"
+        print match.group(3)
+        print match.group(4)
+        print match.group(5)
+    else:
+            print "No Match"
