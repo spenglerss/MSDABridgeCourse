adfd4b54f181cadbf4bc8c8d8646cbe45327945c
spenglerss <sandeshsadalge@gmail.com>
2014-09-02 23:05:14 -0400

    Fall 14 Week 1

---
+++ IS 607/HW 1.R
@@ -0,0 +1,10 @@
+# Question 1
+print(version)
+
+# Question 2
+# PrestgreSQL version: PostgreSQL (64 bit) v9.3.5-1
+
+# Question 3
+require (DMwR)
+blah <- sales
+# According to R Studi0, there are 401,146 observations of 5 variables 
