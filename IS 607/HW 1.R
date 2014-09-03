27277e68b99b6ab5782a3cbfcff37ef8f9b1f25a
spenglerss <sandeshsadalge@gmail.com>
2014-09-02 23:13:12 -0400

    Fall 14 HW 1

--- IS 607/HW 1.R
+++ IS 607/HW 1.R
@@ -1,5 +1,6 @@
 # Question 1
 print(version)
+# Output of above said:  R version 3.1.1 (2014-07-10) "Sock it to Me"
 
 # Question 2
 # PrestgreSQL version: PostgreSQL (64 bit) v9.3.5-1
@@ -8,3 +9,7 @@ print(version)
 require (DMwR)
 blah <- sales
 # According to R Studi0, there are 401,146 observations of 5 variables 
+
+
+
+print(version)
